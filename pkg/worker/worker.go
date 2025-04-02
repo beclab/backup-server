ba086cee2bdcc97044f410484d10c05c1e88a555
aby913 <aby913@163.com>
2025-04-02 19:37:27 +0800

    fix: worker management enhancement

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -26,7 +26,7 @@ type WorkerManage struct {
 	activeBackup  *activeBackup
 	activeRestore *activeRestore
 
-	queueMutex sync.Mutex
+	sync.Mutex
 }
 
 type activeBackup struct {
@@ -88,38 +88,54 @@ func (w *WorkerManage) StartRestoreWorker() {
 }
 
 func (w *WorkerManage) RunRestore(ctx context.Context) {
-	if w.activeRestore != nil {
-		log.Infof("[worker] active restore %s is running", w.activeRestore.restore)
+	var restoreId string
+	var ok bool
+	var setupErr error
+
+	func() {
+		w.Lock()
+		defer w.Unlock()
+
+		if w.activeRestore != nil {
+			log.Infof("[worker] active restore %s is running, skip", w.activeRestore.restoreId)
+			setupErr = fmt.Errorf("[worker] active restore %s is running, skip", w.activeRestore.restoreId)
+			return
+		}
+
+		restoreId, ok = w.getRestoreQueue()
+		if !ok {
+			setupErr = fmt.Errorf("[worker] restoreQueue is empty")
+			return
+		}
+
+		var ctxTask, cancelTask = context.WithCancel(ctx)
+
+		var storageRestore = &storage.StorageRestore{
+			Ctx:       ctxTask,
+			Handlers:  w.handlers,
+			RestoreId: restoreId,
+		}
+
+		w.activeRestore = &activeRestore{
+			ctx:       ctx,
+			cancel:    cancelTask,
+			restoreId: restoreId,
+			restore:   storageRestore,
+			progress:  0.0,
+		}
+	}()
+
+	if setupErr != nil {
 		return
 	}
 
-	restoreId, ok := w.getRestoreQueue()
-	if !ok {
-		return
-	}
-
-	var ctxTask, cancelTask = context.WithCancel(ctx)
-
-	var storageRestore = &storage.StorageRestore{
-		Ctx:       ctxTask,
-		Handlers:  w.handlers,
-		RestoreId: restoreId,
-	}
-
-	w.activeRestore = &activeRestore{
-		ctx:       ctx,
-		cancel:    cancelTask,
-		restoreId: restoreId,
-		restore:   storageRestore,
-		progress:  0.0,
-	}
 	log.Infof("[worker] run restore %s", restoreId)
 
 	if err := w.activeRestore.restore.RunRestore(w.callbackRestoreProgress); err != nil {
 		log.Errorf("[worker] restore %s error: %v", restoreId, err)
 	}
 
-	w.clearActiveRestore()
+	w.clearActiveRestore(restoreId)
 }
 
 func (w *WorkerManage) callbackRestoreProgress(percentDone float64) {
@@ -131,138 +147,119 @@ func (w *WorkerManage) callbackRestoreProgress(percentDone float64) {
 }
 
 func (w *WorkerManage) RunBackup(ctx context.Context) {
-	if w.activeBackup != nil {
-		log.Infof("[worker] active snapshot %s is running", w.activeBackup.snapshotId)
+	var backupId string
+	var snapshotId string
+	var ok bool
+	var setupErr error
+
+	func() {
+		w.Lock()
+		defer w.Unlock()
+
+		if w.activeBackup != nil {
+			log.Infof("[worker] active snapshot %s is running, skip", w.activeBackup.snapshotId)
+			setupErr = fmt.Errorf("[worker] active snapshot %s is running, skip", w.activeBackup.snapshotId)
+			return
+		}
+
+		_, backupId, snapshotId, ok = w.getBackupQueue()
+		if !ok {
+			setupErr = fmt.Errorf("[worker] backupQueue is empty")
+			return
+		}
+
+		var ctxTask, cancelTask = context.WithCancel(ctx)
+		var storageBackup = &storage.StorageBackup{
+			Ctx:        ctxTask,
+			Handlers:   w.handlers,
+			SnapshotId: snapshotId,
+		}
+
+		w.activeBackup = &activeBackup{
+			ctx:        ctx,
+			cancel:     cancelTask,
+			backupId:   backupId,
+			snapshotId: snapshotId,
+			backup:     storageBackup,
+		}
+	}()
+
+	if setupErr != nil {
 		return
 	}
 
-	_, backupId, snapshotId, ok := w.getBackupQueue()
-	if !ok {
-		return
-	}
-
-	var ctxTask, cancelTask = context.WithCancel(ctx)
-
-	var storageBackup = &storage.StorageBackup{
-		Ctx:        ctxTask,
-		Handlers:   w.handlers,
-		SnapshotId: snapshotId,
-	}
-
-	w.activeBackup = &activeBackup{
-		ctx:        ctx,
-		cancel:     cancelTask,
-		backupId:   backupId,
-		snapshotId: snapshotId,
-		backup:     storageBackup,
-	}
-
-	log.Infof("[worker] run backup %s snapshot %s", backupId, snapshotId)
+	log.Infof("[worker] run backup: %s, snapshot: %s", backupId, snapshotId)
 
 	if err := w.activeBackup.backup.RunBackup(); err != nil {
-		log.Errorf("[worker] backup %s error: %v", snapshotId, err)
+		log.Errorf("[worker] backup: %s, error: %v", snapshotId, err)
 	}
 
-	w.clearActiveBackup()
+	w.clearActiveBackup(backupId, snapshotId)
 }
 
 func (w *WorkerManage) CancelBackup(backupId string) error {
-	if w.activeBackup == nil {
-		return fmt.Errorf("no snapshot is running")
-	}
+	w.Lock()
+	defer w.Unlock()
 
-	if w.isBackupQueueEmpty() { // CancelBackup
-		return fmt.Errorf("backupQueue is empty")
-	}
-
-	if w.activeBackup.backupId != backupId {
-		return nil
-	}
+	log.Infof("[worker] cancel backup: %s", backupId)
 
 	w.removeBackupSnapshotsFromBackupQueue(backupId)
 
-	w.activeBackup.cancel()
+	if w.activeBackup != nil && w.activeBackup.backupId == backupId {
+		w.activeBackup.cancel()
 
-	w.clearActiveBackup()
+		w.activeBackup = nil
+	}
 
 	return nil
 }
 
 func (w *WorkerManage) CancelSnapshot(snapshotId string) error {
-	if w.activeBackup == nil {
-		return fmt.Errorf("no snapshot is running")
+	w.Lock()
+	defer w.Unlock()
+
+	log.Infof("[worker] cancel snapshot: %s", snapshotId)
+
+	w.removeSnapshotIdFromBackupQueue(snapshotId)
+
+	if w.activeBackup != nil && w.activeBackup.snapshotId == snapshotId {
+		w.activeBackup.cancel()
+
+		w.activeBackup = nil
 	}
-
-	if w.isBackupQueueEmpty() { // CancelSnapshot
-		return fmt.Errorf("backupQueue is empty")
-	}
-
-	if w.activeBackup.snapshotId != snapshotId {
-		if ok := w.removeSnapshotIdFromBackupQueue(snapshotId); !ok {
-			log.Infof("snapshot %s not in backupQueue", snapshotId)
-		} else {
-			log.Infof("snapshot %s removed from backupQueue", snapshotId)
-		}
-		return nil
-	}
-
-	w.activeBackup.cancel()
-
-	w.clearActiveBackup()
 
 	return nil
 }
 
 func (w *WorkerManage) CancelRestore(restoreId string) error {
-	if w.activeRestore == nil {
-		return fmt.Errorf("no restore is running")
+	w.Lock()
+	defer w.Unlock()
+
+	log.Infof("[worker] cancel restore: %s", restoreId)
+
+	w.removeRestoreIdFromRestoreQueue(restoreId)
+
+	if w.activeRestore != nil && w.activeRestore.restoreId == restoreId {
+		w.activeRestore.cancel()
+
+		w.activeRestore = nil
 	}
-
-	if w.isRestoreQueueEmpty() {
-		return fmt.Errorf("restoreQueue is empty")
-	}
-
-	if w.activeRestore.restoreId != restoreId {
-		if ok := w.removeRestoreIdFromRestoreQueue(restoreId); !ok {
-			log.Infof("restore %s not in restoreQueue", restoreId)
-		} else {
-			log.Infof("restore %s removed from restoreQueue", restoreId)
-		}
-		return nil
-	}
-
-	w.activeRestore.cancel()
-	// TODO update snapshot Phase
-	// TODO if Completed or Failed, skip
-
-	w.clearActiveRestore()
 
 	return nil
 }
 
 func (w *WorkerManage) AppendBackupTask(id string) {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
+	w.Lock()
+	defer w.Unlock()
 
 	w.backupQueue = append(w.backupQueue, id)
 }
 
 func (w *WorkerManage) AppendRestoreTask(restoreId string) {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
+	w.Lock()
+	defer w.Unlock()
 
 	w.restoreQueue = append(w.restoreQueue, restoreId)
-}
-
-func (w *WorkerManage) StopBackup(snapshotId string) {
-	if w.activeBackup.snapshotId != snapshotId {
-		log.Warnf("active snapshot %s is not equal %s", w.activeBackup.snapshotId, snapshotId)
-		return
-	}
-
-	log.Infof("stop backup %s", snapshotId)
-	w.activeBackup.cancel()
-	w.activeBackup = nil
 }
 
 func (w *WorkerManage) GetRestoreProgress(restoreId string) (float64, error) {
@@ -276,22 +273,21 @@ func (w *WorkerManage) GetRestoreProgress(restoreId string) (float64, error) {
 	return w.activeRestore.progress, nil
 }
 
-func (w *WorkerManage) clearActiveBackup() {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
+func (w *WorkerManage) clearActiveBackup(backupId, snapshotId string) {
+	w.Lock()
+	defer w.Unlock()
+	log.Infof("[worker] clear active backup: %s, snapshot: %s", backupId, snapshotId)
 	w.activeBackup = nil
 }
 
-func (w *WorkerManage) clearActiveRestore() {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
+func (w *WorkerManage) clearActiveRestore(restoreId string) {
+	w.Lock()
+	defer w.Unlock()
+	log.Infof("[worker] clear active restore: %s", restoreId)
 	w.activeRestore = nil
 }
 
 func (w *WorkerManage) getBackupQueue() (owner string, backupId string, snapshotId string, flag bool) {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-
 	if len(w.backupQueue) == 0 {
 		return
 	}
@@ -313,9 +309,6 @@ func (w *WorkerManage) getBackupQueue() (owner string, backupId string, snapshot
 }
 
 func (w *WorkerManage) getRestoreQueue() (string, bool) {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-
 	if len(w.restoreQueue) == 0 {
 		return "", false
 	}
@@ -326,22 +319,7 @@ func (w *WorkerManage) getRestoreQueue() (string, bool) {
 	return first, true
 }
 
-func (w *WorkerManage) isBackupQueueEmpty() bool {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-	return len(w.backupQueue) == 0
-}
-
-func (w *WorkerManage) isRestoreQueueEmpty() bool {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-	return len(w.restoreQueue) == 0
-}
-
 func (w *WorkerManage) removeBackupSnapshotsFromBackupQueue(backupId string) bool {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-
 	if len(w.backupQueue) == 0 {
 		return false
 	}
@@ -363,9 +341,6 @@ func (w *WorkerManage) removeBackupSnapshotsFromBackupQueue(backupId string) boo
 }
 
 func (w *WorkerManage) removeSnapshotIdFromBackupQueue(snapshotId string) bool {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-
 	if len(w.backupQueue) == 0 {
 		return false
 	}
@@ -394,9 +369,6 @@ func (w *WorkerManage) removeSnapshotIdFromBackupQueue(snapshotId string) bool {
 }
 
 func (w *WorkerManage) removeRestoreIdFromRestoreQueue(restoreId string) bool {
-	w.queueMutex.Lock()
-	defer w.queueMutex.Unlock()
-
 	if len(w.restoreQueue) == 0 {
 		return false
 	}
